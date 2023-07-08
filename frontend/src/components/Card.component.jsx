2cdced3eb644e361a1bd91b7d379856a1e18e1f7
Serena <serena.dean@autozone.com>
2023-07-08 17:33:24 -0500

    feat: implements search and fixes to status filter

--- frontend/src/components/Card.component.jsx
+++ frontend/src/components/Card.component.jsx
@@ -8,45 +8,66 @@ import { useContext, useEffect, useState } from "react";
 import { Store } from "../context/store.context";
 
 const Card = () => {
-    const [data, setData] = useState();
-    const [text, setText] = useState('');
     const {isCardFlipped} = useContext(CardContext);
 	const { state } = useContext(Store);
     const { suggested, steam } = state;
+    const [data, setData] = useState();
+    const [text, setText] = useState('');
+    const [status, setStatus] = useState('next'); 
 
     const handleSearch = (e) => {
-        let dataCopy
+        let dataCopy = isCardFlipped ? steam.data : suggested.data
         let text = e.target.value
         setText(text)
-
-        const filteredData = data.filter(game => game.name.toLowerCase().includes(text.toLowerCase()))
-
+        
         if (text === '') {
-            // If the card is on suggested, it will set data to keep showing suggested
-            if (!isCardFlipped) {
-                dataCopy = suggested.data
-                setData(dataCopy)
-                return
-            }
-            dataCopy = steam.data
-            setData(dataCopy)
-            return
+            sortStatus(status)
+        } else {
+            let results = dataCopy.filter((game) => {
+                return game.name.toLowerCase().includes(text.toLowerCase())
+            })
+            setData(results)
         }
-        setData(filteredData)
     }
 
-    // Temp workaround:
+    const handleStatus = (e) => {
+        let status = e.target.value
+        setStatus(status)
+        sortStatus(status)
+    }
+
+    const sortStatus = (status) => {
+        let dataCopy = isCardFlipped ? steam.data : suggested.data
+        if (dataCopy === suggested.data) {
+            let results = dataCopy.sort((a, b) => {
+                if (a.status === status && b.status !== status) return -1;
+                if (a.status !== status && b.status === status) return 1;
+                return a.status.localeCompare(b.status);
+            }) 
+            
+            setData(results)
+            return
+        }
+        setData(dataCopy)
+    }
+
+    // ! Temp workaround:
     useEffect(() => {
         setData(suggested.data)
     },[suggested])
 
     useEffect(() => {
-        setData(data === suggested.data ? steam.data : suggested.data);
+        if (data === suggested.data || isCardFlipped) {
+            setData(steam.data)
+        } else if (data === steam.data || !isCardFlipped) {
+            setData(suggested.data)
+        }
+        setText('')
     }, [isCardFlipped])
     
     return (
         <div id="card">
-            <CardHeader />
+            <CardHeader handleStatus={handleStatus} />
             <CardInputGroup handleSearch={handleSearch} text={text} />
             <CardBody data={data} />
             <CardFooter />
@@ -56,28 +77,26 @@ const Card = () => {
 
 export default Card;
 
-const CardHeader = () => {
+const CardHeader = ({handleStatus}) => {
     const {cardHeader, isCardFlipped} = useContext(CardContext);
-    const [statusSelection, setStatusSelection] = useState("next"); 
-
 
     return (
         <div id="card-header-container">
             <h3 id="card-header">{cardHeader}</h3>
-            {/* {
+            {
             isCardFlipped ?
                 null 
             :
                 <div id="status-container">
                     <label htmlFor="status">Sort by:</label>
-                    <select name="status" id="status-selection" onChange={(event) => setStatusSelection(event.target.value)}>
+                    <select name="status" id="status-selection" defaultValue="next" onChange={handleStatus}>
                         <option value="next">Playing Next</option>
                         <option value="queue">In Queue</option>
                         <option value="completed">Completed</option>
                         <option value="declined">Declined</option>
                     </select>
                 </div>
-            } */}
+            }
         </div>
     )
 }
@@ -99,8 +118,8 @@ const CardBody = ({data}) => {
 }
 
 const CardList = ({data}) => {
-    const {isCardFlipped} = useContext(CardContext);
-
+    const { isCardFlipped } = useContext(CardContext);
+    
     return (
         <div className="card-list">
             { data?.map(({name, image, username, status}) =>
