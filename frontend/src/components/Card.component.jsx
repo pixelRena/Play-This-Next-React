0b9269b5164175f5dc6ab4ce4cfb87e5c2020255
Serena <serena.dean@autozone.com>
2023-07-08 14:01:00 -0500

    feat: implements search functionality of games

--- frontend/src/components/Card.component.jsx
+++ frontend/src/components/Card.component.jsx
@@ -7,13 +7,52 @@ import { CardContext } from "../context/card.context";
 import { useContext, useEffect, useState } from "react";
 import { Store } from "../context/store.context";
 
-const Card = () => (
-    <div id="card">
-        <CardBody />
-        <CardInputGroup/>
-        <CardFooter/>
-    </div>
-)
+const Card = () => {
+    const [data, setData] = useState();
+    const [text, setText] = useState('');
+    const {isCardFlipped} = useContext(CardContext);
+	const { state } = useContext(Store);
+    const { suggested, steam } = state;
+
+    const handleSearch = (e) => {
+        let dataCopy
+        let text = e.target.value
+        setText(text)
+
+        const filteredData = data.filter(game => game.name.toLowerCase().includes(text.toLowerCase()))
+
+        if (text === '') {
+            // If the card is on suggested, it will set data to keep showing suggested
+            if (!isCardFlipped) {
+                dataCopy = suggested.data
+                setData(dataCopy)
+                return
+            }
+            dataCopy = steam.data
+            setData(dataCopy)
+            return
+        }
+        setData(filteredData)
+    }
+
+    // Temp workaround:
+    useEffect(() => {
+        setData(suggested.data)
+    },[suggested])
+
+    useEffect(() => {
+        setData(data === suggested.data ? steam.data : suggested.data);
+    }, [isCardFlipped])
+    
+    return (
+        <div id="card">
+            <CardHeader />
+            <CardInputGroup handleSearch={handleSearch} text={text} />
+            <CardBody data={data} />
+            <CardFooter />
+        </div>    
+    )
+}
 
 export default Card;
 
@@ -21,10 +60,11 @@ const CardHeader = () => {
     const {cardHeader, isCardFlipped} = useContext(CardContext);
     const [statusSelection, setStatusSelection] = useState("next"); 
 
+
     return (
         <div id="card-header-container">
             <h3 id="card-header">{cardHeader}</h3>
-            {
+            {/* {
             isCardFlipped ?
                 null 
             :
@@ -37,25 +77,14 @@ const CardHeader = () => {
                         <option value="declined">Declined</option>
                     </select>
                 </div>
-            }
+            } */}
         </div>
     )
 }
 
-const CardBody = () => {
-    const {isCardFlipped} = useContext(CardContext);
+const CardBody = ({data}) => {
 	const { state } = useContext(Store);
     const { suggested, steam } = state;
-    const [data, setData] = useState();
-
-    // Temp workaround:
-    useEffect(() => {
-        setData(suggested.data)
-    },[suggested])
-
-    useEffect(() => {
-        setData(data === suggested.data ? steam.data : suggested.data);
-    },[isCardFlipped])
 
     return (
         <div id="card-body">
@@ -117,13 +146,13 @@ const CardFooter = () => {
     return <div id="card-footer">{cardFooter} {isCardFlipped ? ownedGames : gamesCompleted}</div>
 }
 
-const CardInputGroup = () => {
+const CardInputGroup = ({handleSearch, text}) => {
     const { setOpen } = useContext(ModalContext);
 
     return ( 
         <>
+            <input value={text} type="text" id="card-search-input" placeholder="Search games.." onChange={handleSearch} />
             <Button variant="add" onClick={() => setOpen(true)}>+</Button>
-            {/* <input type="text" id="card-search-input" placeholder="Search games.." /> */}
         </>
     )
 }
