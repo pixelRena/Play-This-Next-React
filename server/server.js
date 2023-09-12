a7109ab5c28d61994e756ac3ec05a6030ec5d392
Serena <serena.dean@autozone.com>
2023-09-11 21:22:25 -0500

    feat: adds update username path

--- server/server.js
+++ server/server.js
@@ -1,21 +1,20 @@
-const express = require('express');
-const axios = require('axios');
+const express = require("express");
+const axios = require("axios");
 const firebase = require("firebase-admin");
-const path = require('path');
+const path = require("path");
 const cors = require("cors");
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
 
 // var serviceAccount = require("./firebaseKey.json");
 firebase.initializeApp({
   credential: firebase.credential.cert({
-    "projectId": process.env.FIREBASE_PROJECT_ID,
-    "clientEmail": process.env.FIREBASE_CLIENT_EMAIL,
-    "privateKey": JSON.parse(process.env.FIREBASE_PRIVATE_KEY),
-    "privateKeyId": process.env.FIREBASE_PRIVATE_KEY_ID,
-    "token_uri":"https://oauth2.googleapis.com/token"
-    }
-    )
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: JSON.parse(process.env.FIREBASE_PRIVATE_KEY),
+    privateKeyId: process.env.FIREBASE_PRIVATE_KEY_ID,
+    token_uri: "https://oauth2.googleapis.com/token",
+  }),
 });
 
 const db = firebase.firestore();
@@ -27,115 +26,152 @@ const docs = db.collection("suggested");
 const app = express();
 
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 
 // Fetches steam games
-app.get('/steam-games', async (req,res) => {
-    try {
-        const {data} = await axios.get("https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key="+process.env.STEAM_WEB_API_KEY+"&steamid="+process.env.STEAM_ID+"&include_appinfo=true&format=json");
-        res.send(data.response);
-    } catch(error) {
-        console.log(error);
-        res.send(error);
-    }
+app.get("/steam-games", async (req, res) => {
+  try {
+    const { data } = await axios.get(
+      "https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=" +
+        process.env.STEAM_WEB_API_KEY +
+        "&steamid=" +
+        process.env.STEAM_ID +
+        "&include_appinfo=true&format=json"
+    );
+    res.send(data.response);
+  } catch (error) {
+    console.log(error);
+    res.send(error);
+  }
 });
 
 // Fetches steam games stored in firebase DB
-app.get('/steam-games-collection', async (req,res) => {   
-    let gamesInDB = [];
+app.get("/steam-games-collection", async (req, res) => {
+  let gamesInDB = [];
 
-    await steamDB.get()
-    .then((snapshot) => {
-        snapshot.forEach(snap => gamesInDB.push(snap.data()))
-    });
-    
-    res.send(gamesInDB);
+  await steamDB.get().then((snapshot) => {
+    snapshot.forEach((snap) => gamesInDB.push(snap.data()));
+  });
+
+  res.send(gamesInDB);
 });
 
 // Updates steam games in firebase DB
-app.post('/seed-steam-games', async (req,res) => {
-    const { gamesList } = req.body;
-    let gamesInDB = [];
+app.post("/seed-steam-games", async (req, res) => {
+  const { gamesList } = req.body;
+  let gamesInDB = [];
 
-    gamesList.map(({appid, img_icon_url, name}) => {    
-        steamDB.doc(String(appid)).set({
-            appid,
-            name,
-            image: "http://media.steampowered.com/steamcommunity/public/images/apps/"+appid+"/"+img_icon_url+".jpg",
-        });
-    })
-    
-    await steamDB.get()
-    .then((snapshot) => {
-        snapshot.forEach(snap => gamesInDB.push(snap.data()))
+  gamesList.map(({ appid, img_icon_url, name }) => {
+    steamDB.doc(String(appid)).set({
+      appid,
+      name,
+      image:
+        "http://media.steampowered.com/steamcommunity/public/images/apps/" +
+        appid +
+        "/" +
+        img_icon_url +
+        ".jpg",
     });
+  });
 
-    res.send(gamesInDB);
+  await steamDB.get().then((snapshot) => {
+    snapshot.forEach((snap) => gamesInDB.push(snap.data()));
+  });
+
+  res.send(gamesInDB);
 });
 
 // Gets results from search input of adding a game
-app.get('/search-games', async (req,res) => {
-    const { term } = req.query;
-    try {
-        const {data} = await axios.get(`https://api.rawg.io/api/games?search=${term}&key=${process.env.RAWG_API_KEY}`, { headers: { "Accept-Encoding": "gzip,deflate,compress" } })
-        let dataReceived = data.results;
-        let dataArr = [];
-        dataReceived.map(({name, released, background_image}) => dataArr.push({
-            "name": name, 
-            "released": released,
-            "image": background_image
-        }));
-        res.send(dataArr);
-    } catch(error) { res.send(error) }
+app.get("/search-games", async (req, res) => {
+  const { term } = req.query;
+  try {
+    const { data } = await axios.get(
+      `https://api.rawg.io/api/games?search=${term}&key=${process.env.RAWG_API_KEY}`,
+      { headers: { "Accept-Encoding": "gzip,deflate,compress" } }
+    );
+    let dataReceived = data.results;
+    let dataArr = [];
+    dataReceived.map(({ name, released, background_image }) =>
+      dataArr.push({
+        name: name,
+        released: released,
+        image: background_image,
+      })
+    );
+    res.send(dataArr);
+  } catch (error) {
+    res.send(error);
+  }
 });
 
 // Gets the suggested games from firebase DB
-app.get('/suggested-games-collection', async(req,res) => {
-    const nextStatusQuery = docs.where("status", "==", "next").orderBy("next");
-    const otherStatusQuery = docs.where("status", "!=", "next");
-    let gamesInDB = [];
+app.get("/suggested-games-collection", async (req, res) => {
+  const nextStatusQuery = docs.where("status", "==", "next").orderBy("next");
+  const otherStatusQuery = docs.where("status", "!=", "next");
+  let gamesInDB = [];
 
-    await nextStatusQuery.get()
-    .then((snapshot) => {
-        snapshot.forEach(snap => gamesInDB.push(snap.data()))
-    });
+  await nextStatusQuery.get().then((snapshot) => {
+    snapshot.forEach((snap) => gamesInDB.push(snap.data()));
+  });
 
-    await otherStatusQuery.get()
-    .then((snapshot) => {
-        snapshot.forEach(snap => gamesInDB.push(snap.data()))
-    });
+  await otherStatusQuery.get().then((snapshot) => {
+    snapshot.forEach((snap) => gamesInDB.push(snap.data()));
+  });
 
-    // console.log(gamesInDB);
-    
-    res.send(gamesInDB);
+  res.send(gamesInDB);
 });
 
 // Adds new suggested game to firebase DB
-app.post('/add-suggested-game', async (req,res) => {
-    const { games, username } = req.body;
-    let updatedSuggestedGames = db.collection("suggested");
-    let gamesInDB = [];
+app.post("/add-suggested-game", async (req, res) => {
+  const { games, username } = req.body;
+  let updatedSuggestedGames = db.collection("suggested");
+  let gamesInDB = [];
 
-    games.map(({name, image}) => {    
-        updatedSuggestedGames.doc(name).set({
-            username: username || "User not provided",
-            name,
-            image,
-            status: "queue",
-        });
+  games.map(({ name, image }) => {
+    updatedSuggestedGames.doc(name).set({
+      username: username || "User not provided",
+      name,
+      image,
+      status: "queue",
     });
+  });
 
-    await updatedSuggestedGames.get()
-    .then((snapshot) => {
-        snapshot.forEach(snap => gamesInDB.push(snap.data()))
-    });
+  await updatedSuggestedGames.get().then((snapshot) => {
+    snapshot.forEach((snap) => gamesInDB.push(snap.data()));
+  });
 
-    res.send(gamesInDB);
+  res.send(gamesInDB);
 });
 
-app.use(express.static(path.join(__dirname, '../frontend/build')));
-app.get("*", (req,res) => 
-	res.sendFile(path.join(__dirname, '../frontend/build/index.html'))
+app.post("/update-username", async (req, res) => {
+  const { newUsername, oldUsername } = req.body;
+
+  const querySnapshot = await docs.where("username", "==", oldUsername).get();
+
+  if (!oldUsername || querySnapshot.empty)
+    return res.status(200).send("No username found");
+
+  const updatePromises = querySnapshot.docs.map(async (snapshot) => {
+    const docRef = snapshot.ref;
+
+    await docRef.update({
+      username: newUsername,
+      image: snapshot.get("image"),
+      name: snapshot.get("name"),
+      status: snapshot.get("status"),
+    });
+  });
+
+  await Promise.all(updatePromises);
+
+  res.status(200).json({ message: "Usernames updated successfully" });
+});
+
+app.use(express.static(path.join(__dirname, "../frontend/build")));
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "../frontend/build/index.html"))
 );
 
-app.listen(process.env.PORT || 3000, () => { console.log("listening on port.."+process.env.PORT)});
+app.listen(process.env.PORT || 3000, () => {
+  console.log("listening on port.." + process.env.PORT);
+});
