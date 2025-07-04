44604f974bab0a2f5ec082debf987e5211abf4cc
Serena <dserena01@yahoo.com>
2025-07-03 20:40:41 -0700

    feat: implement dropdowns for sort and filter, import bootstrap styles

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -18,12 +18,38 @@ const App = () => {
           <button type="button" className="btn" disabled>
             Add a game
           </button>
-          <button type="button" className="btn">
-            Sort by
-          </button>
-          <button type="button" className="btn">
-            Filter by
-          </button>
+          <div className="btn btn-group dropend">
+            <button
+              type="button"
+              className="btn bg-transparent dropdown-toggle"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              Sort by
+            </button>
+            <ul className="dropdown-menu ms-3 py-0">
+              <li className="dropdown-item">> Currently Playing</li>
+              <li className="dropdown-item">> Queue</li>
+              <li className="dropdown-item">> Completed</li>
+              <li className="dropdown-item">> Declined</li>
+            </ul>
+          </div>
+          <div className="btn btn-group dropend">
+            <button
+              type="button"
+              className="btn bg-transparent dropdown-toggle"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              Filter by
+            </button>
+            <ul className="dropdown-menu ms-3 py-0">
+              <li className="dropdown-item">> Currently Playing</li>
+              <li className="dropdown-item">> Queue</li>
+              <li className="dropdown-item">> Completed</li>
+              <li className="dropdown-item">> Declined</li>
+            </ul>
+          </div>
           <button
             type="button"
             className="btn"
--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -6,11 +6,13 @@ import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
 // import { NotificationContextProvider } from "components/notification/Notification.context"
 // import { ModalContextProvider } from "components/modal/Modal.context"
 // import { StoreProvider } from "context/Store.context"
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap.bundle.min.js"
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 
 root.render(
-  <React.StrictMode>
+  <>
     {/* <StoreProvider>
       <ModalContextProvider>
         <NotificationContextProvider> */}
@@ -18,5 +20,5 @@ root.render(
     {/* </NotificationContextProvider>
       </ModalContextProvider>
     </StoreProvider> */}
-  </React.StrictMode>
+  </>
 )
