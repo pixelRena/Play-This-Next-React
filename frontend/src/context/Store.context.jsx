90788d76079b85e938e28ed869bebfe514008179
Serena <dserena01@yahoo.com>
2025-11-06 01:35:31 -0800

    chore: update callback link

--- frontend/src/context/Store.context.jsx
+++ frontend/src/context/Store.context.jsx
@@ -81,7 +81,7 @@ export const StoreProvider = ({ children }) => {
 
   const authorize = async () =>
     window.location.replace(
-      `https://id.twitch.tv/oauth2/authorize?client_id=8h55e8b7evg28b8f1ybsb3sin8b883&redirect_uri=https://play-this-next-react.vercel.app/callback&response_type=token&scope=user_read`
+      `https://id.twitch.tv/oauth2/authorize?client_id=8h55e8b7evg28b8f1ybsb3sin8b883&redirect_uri=https://pixelrena.github.io/Play-This-Next-React/callback&response_type=token&scope=user_read`
     )
 
   const value = {
